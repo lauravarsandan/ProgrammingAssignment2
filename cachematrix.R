e55b82b91746468df48fe1ac74995c1ecdc7e1bc
lauravarsandan <laura.varsandan@gmail.com>
2016-12-04 21:06:00 +0000

    cache inverse of matrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Matrix inversion is usually a costly computation and there may be some benefit 
+##to caching the inverse of a matrix rather than compute it repeatedly 
+##(there are also alternatives to matrix inversion that we will not discuss here). 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+                {
+                  inverse<-NULL
+                  set<-function(y)
+                        {
+                          x<<-y
+                          inverse<<-NULL
+                        }
+                  get<- function() x
+                  setinverse<-function(solve) inverse<<-solve(x)
+                  getinverse<-function() inverse
+                  list(set=set,get=get, setinverse=setinverse, getinverse=getinverse)
+                }
 
+
+## This function checks if there is a cached inverse for a matrix. 
+## If there is, it prints the cached results, if not, it calculates it and caches the results
+
+cacheSolve <- function(x, ...) 
+              {
+                inverse<-x$getinverse()
+                if(!is.null(inverse))
+                {
+                  message("getting cached data")
+                  return(inverse)
+                }
+                data<-x$get()
+                inverse<-solve(data,...)
+                x$setinverse(inverse)
+                inverse
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## test the code
+x<-matrix(data=rnorm(10),2,2)
+x
+solve(x)
+makeCacheMatrix(x)
+cacheSolve(makeCacheMatrix(x))
